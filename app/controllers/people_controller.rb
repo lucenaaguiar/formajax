6f8fa23107bb59fbf6798717f747c82b31f7bbfd
Leonardo Lucena <lucenaaguiar@gmail.com>
2016-12-13 14:11:09 -0200

    formulario ajax rails 5

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,20 @@
+class PeopleController < ApplicationController
+  respond_to :js, :html
+
+  def index
+  @person = Person.new
+  @people = Person.all
+  end
+
+  def create
+  @person = Person.new(person_params)
+  @person.save
+  end
+
+  private
+
+  # strong parameters
+  def person_params
+  params.require(:person).permit(:name, :email)
+  end
+end
